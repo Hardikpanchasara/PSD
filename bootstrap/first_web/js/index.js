242897e743213870beb18da8dd5b09ff8a26e330
Hardikpanchasara <hardikpatel97253@gmail.com>
2023-02-15 10:39:29 +0530

    complete walert

---
+++ bootstrap/first_web/js/index.js
@@ -0,0 +1,23 @@
+// pre loader
+window.addEventListener("load", function() {
+    const preloader = document.querySelector(".preloader")
+    preloader.style.opacity = '0';
+
+    setTimeout(() => {
+        preloader.style.display = "none";
+    }, 10);
+
+})
+
+// tooltip
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// toast
+document.getElementById("toastbtn").onclick = function () {
+    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+    var toastList = toastElList.map(function (toastEl) {
+        return new bootstrap.Toast(toastEl)
+    })
+    toastList.forEach(toast => toast.show())
+}
---
+++ bootstrap/walert/js/index.js
@@ -0,0 +1,10 @@
+// pre loader
+window.addEventListener("load", function() {
+    const preloader = document.querySelector(".preloader")
+    preloader.style.opacity = '0';
+
+    setTimeout(() => {
+        preloader.style.display = "none";
+    }, 10);
+
+})
