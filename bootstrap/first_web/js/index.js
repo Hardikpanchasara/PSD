2ec4a7af2ee01ad9ba70c820db35f23254e1cb29
Hardikpanchasara <hardikpatel97253@gmail.com>
2023-03-29 10:17:23 +0530

    update

--- bootstrap/first_web/js/index.js
+++ bootstrap/first_web/js/index.js
@@ -1,11 +1,11 @@
 // pre loader
 window.addEventListener("load", function() {
     const preloader = document.querySelector(".preloader")
+    
     preloader.style.opacity = '0';
-
     setTimeout(() => {
         preloader.style.display = "none";
-    }, 10);
+    }, 10000);
 
 })
 
