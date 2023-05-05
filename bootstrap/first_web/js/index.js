0d4d81f9da400edfec42cac88730203010200bbc
Hardikpanchasara <hardikpatel97253@gmail.com>
2023-05-05 22:51:22 +0530

    modified files

--- bootstrap/first_web/js/index.js
+++ bootstrap/first_web/js/index.js
@@ -5,7 +5,7 @@ window.addEventListener("load", function() {
     preloader.style.opacity = '0';
     setTimeout(() => {
         preloader.style.display = "none";
-    }, 10000);
+    }, 1);
 
 })
 
